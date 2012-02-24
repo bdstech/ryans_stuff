35e6de1baa10a810f89ca7a1558b3b0b373650ab
Ryan McCue <techsupport@bulldogsystems.com>
2012-02-24 14:55:12 -0500

    tstuff for sma

---
+++ alter_table.sql
@@ -0,0 +1,7 @@
+
+ALTER TABLE project_298_15_minute_measured ENGINE=INNODB;
+ALTER TABLE project_298_calculated ENGINE=INNODB;
+ALTER TABLE project_298_daily_calculated ENGINE=INNODB;
+ALTER TABLE project_226_15_minute_measured ENGINE=INNODB;
+ALTER TABLE project_226_calculated ENGINE=INNODB;
+ALTER TABLE project_226_daily_calculated ENGINE=INNODB;
